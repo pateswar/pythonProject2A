128281a564ba858fe797bdac24ec761f20da3806
pateswar <pateswar@oregonstate.edu>
2022-01-12 20:14:15 -0800

    Initial commit

---
+++ average.py
@@ -0,0 +1,8 @@
+print("Please enter five numbers.")
+x1 = float(input())
+x2 = float(input())
+x3 = float(input())
+x4 = float(input())
+x5 = float(input())
+x = x1+x2+x3+x4+x5
+print("The average of those numbers is:", x/5)
---
+++ temp_convert.py
@@ -0,0 +1,4 @@
+print("Please enter a Celsius temperature.")
+x = float(input())
+print("The equivalent Fahrenheit temperature is:")
+print ((9/5)*x +32 )
